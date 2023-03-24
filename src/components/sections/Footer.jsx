eae3e0a99c3bc145aca1a2efce91a8265e4d66b2
chaarz <cduppre@gmail.com>
2023-03-24 17:19:27 -0400

    added footer sections

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,7 +1,16 @@
 const Footer = () => {
     return (
         <footer>
-            <p>Most Complete job portal</p>
+            <div className='wrapper'>
+                <div>
+                    <h2>Most Complete job portal</h2>
+                </div>
+                <div className="bottomFooter">
+                    <p>Privacy & Terms</p>
+                    <p>Contact Us</p>
+                    <p>Copyright @2023 jobi inc.</p>
+                </div>
+            </div>
         </footer>
     )
   }
