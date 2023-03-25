a16a8db70fac11266d7b6c3fdfc197240d59eb69
chaarz <cduppre@gmail.com>
2023-03-25 15:41:54 -0400

    added and styled upper footer section

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,14 +1,29 @@
 const Footer = () => {
     return (
         <footer>
-            <div className='wrapper'>
-                <div>
-                    <h2>Most Complete job portal</h2>
+            <div className='upperFooter'>
+                <div className='wrapper'>
+                    <div className='flexContainer'>
+                        <div className='headingContainer'>
+                            <h2>Complete Job Portal for Everyone</h2>
+                        </div>
+                        <div className='flexTextContainer'>
+                            <p>Find your dream job or your next talent.</p>
+                            <div className='flexBtnContainer'>
+                                <button className='lookingBtn'>Looking for a job?</button>
+                                <button>Post a job</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="bottomFooter">
-                    <p>Privacy & Terms</p>
-                    <p>Contact Us</p>
-                    <p>Copyright @2023 jobi inc.</p>
+            </div>
+            <div className='mainFooter'>
+                <div className='wrapper'>
+                    <div className="bottomFooter">
+                        <p>Privacy & Terms</p>
+                        <p>Contact Us</p>
+                        <p>Copyright @2023 jobi inc.</p>
+                    </div>
                 </div>
             </div>
         </footer>
