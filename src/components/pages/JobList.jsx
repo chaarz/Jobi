8c4ab7631edd6bb8921ff98841c14e54fea8a638
chaarz <cduppre@gmail.com>
2023-03-30 10:22:48 -0400

    moved job samples to separate component

--- src/components/pages/JobList.jsx
+++ src/components/pages/JobList.jsx
@@ -1,111 +1,14 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import images from '../images';
+import JobSamples from '../JobSamples';
 import { getDatabase, ref, onValue, push, set } from 'firebase/database';
 import firebase from '../firebase';
 
 const JobList = () => {
     const [jobs, setJobs] = useState([]);
     // sample job object
-    const jobSample = [  {
-        uid: "0001",
-        url: 'https://omega.corp/jobs/full-stack-developer',
-        benefits: [
-          'Competitive salary and benefits package with annual bonuses.',
-          'Flexible work hours and options to work remotely from anywhere in the world.',
-          'Opportunities for career growth and advancement.',
-          'Collaborative and fun work environment with regular team events.',
-        ],
-        category: 'Development',
-        date: '2023-03-27',
-        employer: 'Omega Corp',
-        facebook: 'https://www.facebook.com/omegacorp',
-        twitter: 'https://twitter.com/omegacorp',
-        experienceYears: 7,
-        expertiseLevel: 'Senior',
-        description:
-          "As a full-stack developer at Omega Corp, you will be responsible for developing and maintaining the company's web applications and services.",
-        title: 'Full-stack Developer',
-        contractType: 'Fulltime',
-        keywords: [
-          'full-stack development',
-          'web development',
-          'HTML',
-          'CSS',
-          'JavaScript',
-          'React',
-          'Node.js',
-          'MongoDB',
-          'AWS',
-        ],
-        location: { city: 'San Francisco', country: 'United States' },
-        overview:
-          'Omega Corp is looking for a highly skilled full-stack developer to join our team.',
-        requiredSkills: [
-          'Expertise in HTML, CSS, and JavaScript.',
-          'Strong experience with modern web development frameworks such as React and Node.js.',
-          'Experience with MongoDB and AWS.',
-          'Excellent problem-solving and debugging skills.',
-          'Exceptional communication and collaboration skills.',
-        ],
-        responsibilities: [
-          "Develop new features for the company's web applications and services.",
-          'Maintain and improve existing code.',
-          'Collaborate with designers and other developers to implement new features.',
-          'Write well-documented, modular, and scalable code.',
-          'Optimize web applications for performance and scalability.',
-        ],
-        salary: { max: 120000, min: 80000 },
-      },  {
-        uid: "0002",
-        url: 'https://omega.corp/jobs/full-stack-developer',
-        benefits: [
-          'Competitive salary and benefits package with annual bonuses.',
-          'Flexible work hours and options to work remotely from anywhere in the world.',
-          'Opportunities for career growth and advancement.',
-          'Collaborative and fun work environment with regular team events.',
-        ],
-        category: 'Development',
-        date: '2023-03-27',
-        employer: 'Omega Corp',
-        facebook: 'https://www.facebook.com/omegacorp',
-        twitter: 'https://twitter.com/omegacorp',
-        experienceYears: 7,
-        expertiseLevel: 'Senior',
-        description:
-          "As a full-stack developer at Omega Corp, you will be responsible for developing and maintaining the company's web applications and services.",
-        title: 'Front-End Developer',
-        contractType: 'Part Time',
-        keywords: [
-          'full-stack development',
-          'web development',
-          'HTML',
-          'CSS',
-          'JavaScript',
-          'React',
-          'Node.js',
-          'MongoDB',
-          'AWS',
-        ],
-        location: { city: 'Toronto', country: 'Canada' },
-        overview:
-          'Omega Corp is looking for a highly skilled full-stack developer to join our team.',
-        requiredSkills: [
-          'Expertise in HTML, CSS, and JavaScript.',
-          'Strong experience with modern web development frameworks such as React and Node.js.',
-          'Experience with MongoDB and AWS.',
-          'Excellent problem-solving and debugging skills.',
-          'Exceptional communication and collaboration skills.',
-        ],
-        responsibilities: [
-          "Develop new features for the company's web applications and services.",
-          'Maintain and improve existing code.',
-          'Collaborate with designers and other developers to implement new features.',
-          'Write well-documented, modular, and scalable code.',
-          'Optimize web applications for performance and scalability.',
-        ],
-        salary: { max: 120000, min: 80000 },
-      }];
+    const jobSample = JobSamples;
 
     useEffect(() => {
         // create a variable that holds our database details
