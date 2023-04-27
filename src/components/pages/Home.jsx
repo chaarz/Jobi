9ec5cc32321f1ebd8706f68eb63b496b34d5d2df
chaarz <cduppre@gmail.com>
2023-04-27 16:48:13 -0400

    added marquee animation to Partners section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -12,26 +12,48 @@ const Home = () => {
             <Header />
             <main>
                 <section className='partners'>
-                    <ul className='wrapper'>
-                        <li>
-                            <img src={images.google} alt="Google logo" />
-                        </li>
-                        <li>
-                            <img src={images.shipbob} alt="ShipBob logo" />
-                        </li>
-                        <li>
-                            <img src={images.dribbble} alt="Dribbble logo" />
-                        </li>
-                        <li>
-                            <img src={images.slack} alt="Slack logo" />
-                        </li>
-                        <li>
-                            <img src={images.vine} alt="Vine logo" />
-                        </li>
-                        <li>
-                            <img src={images.airbnb} alt="Airbnb logo" />
-                        </li>
-                    </ul>
+                    <div className='wrapper marquee-container'>
+                        <ul>
+                            <li>
+                                <img src={images.google} alt="Google logo" />
+                            </li>
+                            <li>
+                                <img src={images.shipbob} alt="ShipBob logo" />
+                            </li>
+                            <li>
+                                <img src={images.dribbble} alt="Dribbble logo" />
+                            </li>
+                            <li>
+                                <img src={images.slack} alt="Slack logo" />
+                            </li>
+                            <li>
+                                <img src={images.vine} alt="Vine logo" />
+                            </li>
+                            <li>
+                                <img src={images.airbnb} alt="Airbnb logo" />
+                            </li>
+                        </ul>
+                        <ul aria-hidden="true">
+                            <li>
+                                <img src={images.google} alt="Google logo" />
+                            </li>
+                            <li>
+                                <img src={images.shipbob} alt="ShipBob logo" />
+                            </li>
+                            <li>
+                                <img src={images.dribbble} alt="Dribbble logo" />
+                            </li>
+                            <li>
+                                <img src={images.slack} alt="Slack logo" />
+                            </li>
+                            <li>
+                                <img src={images.vine} alt="Vine logo" />
+                            </li>
+                            <li>
+                                <img src={images.airbnb} alt="Airbnb logo" />
+                            </li>
+                        </ul>
+                    </div>
                 </section>
                 <section className='categories wrapper'>
                     <div className='sectionHeading'>
