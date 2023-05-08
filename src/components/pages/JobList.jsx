5d61e01a293843cb3d9a2e48177af43e3bc7e341
chaarz <cduppre@gmail.com>
2023-05-08 14:25:17 -0400

    refactored jobList markup and css for responsiveness

--- src/components/pages/JobList.jsx
+++ src/components/pages/JobList.jsx
@@ -85,36 +85,36 @@ const JobList = () => {
                             return (
                                 <li key={job.id}>
                                     <Link to={`/jobList/${job.id}`}>
-                                        <div className='jobTitle'>
-                                            <div className='companyLogo'>
-                                                <img src={images.profile} alt="company logo" />
-                                            </div>
+                                        <div className='companyLogo'>
+                                            <img src={images.profile} alt="company logo" />
+                                        </div>
+                                        <div className='jobCard'>
                                             <h4>{job.title}</h4>
-                                        </div>
-                                        <div className='jobCardType flex'>
-                                            <p
-                                                style={
-                                                    job.contractType === "Full-time"
-                                                        ? { color: "#008000" }
-                                                        : job.contractType === "Part-time"
-                                                            ? { color: "#ff0000" }
-                                                            : null
-                                                }
-                                            >
-                                                {job.contractType}
-                                            </p>
-                                            <p>{toCurrency(job.salary.min)} - {toCurrency(job.salary.max)}</p>
-                                        </div>
-                                        <div className='jobCardDetails flex'>
-                                            <p className='jobLocation'>{job.location.city}, {job.location.country}</p>
-                                            <p>{job.category}</p>
+                                            <div className='jobCardType flex'>
+                                                <p
+                                                    style={
+                                                        job.contractType === "Full-time"
+                                                            ? { color: "#008000" }
+                                                            : job.contractType === "Part-time"
+                                                                ? { color: "#ff0000" }
+                                                                : null
+                                                    }
+                                                >
+                                                    {job.contractType}
+                                                </p>
+                                                <p>{toCurrency(job.salary.min)} - {toCurrency(job.salary.max)}</p>
+                                            </div>
+                                            <div className='jobCardDetails flex'>
+                                                <p className='jobLocation'>{job.location.city}, {job.location.country}</p>
+                                                <p>{job.category}</p>
+                                            </div>
                                         </div>
                                         <div className='jobCardBtn'>
                                             <button onClick={handleClickOpen} className='saveJobBtn highlight'>
                                                 <span className="sr-only">Bookmark job</span>
                                                 <FontAwesomeIcon icon={faBookmark} />
                                             </button>
-                                            <button>APPLY</button>
+                                            <button className="apply">APPLY</button>
                                         </div>
                                     </Link>
                                 </li>
